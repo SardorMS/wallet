24ec9b4a3c1f4a8b9b982a797a8f77fad12bf82f
SardorMS <mshinodawindows@gmail.com>
2021-04-19 00:49:35 +0500

    added Import func

--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -2,9 +2,11 @@ package wallet
 
 import (
 	"errors"
+	"io"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/SardorMS/wallet/pkg/types"
 	"github.com/google/uuid"
@@ -218,6 +220,74 @@ func (s *Service) ExportToFile(path string) error {
 	if err != nil {
 		log.Print(err)
 		return err
+	}
+	return nil
+}
+
+//ImportToFile - import(reads) from file to accounts.
+func (s *Service) ImportToFile(path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+	defer func() {
+		if cerr := file.Close(); cerr != nil {
+			log.Print(cerr)
+		}
+	}()
+
+	content := make([]byte, 0)
+	buf := make([]byte, 4)
+	for {
+		read, err := file.Read(buf)
+		if err == io.EOF {
+			content = append(content, buf[:read]...)
+			break
+		}
+
+		if err != nil {
+			log.Print(err)
+			return err
+		}
+		content = append(content, buf[:read]...)
+	}
+
+	data := string(content)
+	log.Println("data: ", data)
+	accounts := strings.Split(data, "|")
+	log.Println("accounts until split: ", accounts)
+
+	// accounts = accounts[:len(accounts) -1]
+	// log.Println("accounts after split: ", accounts)
+
+	for _, operation := range accounts {
+		strAcc := strings.Split(operation, ";")
+		log.Println("strAcc:", strAcc)
+
+		id, err := strconv.ParseInt(strAcc[0], 10, 64)
+		if err != nil {
+			log.Print(err)
+			return err
+		}
+
+		phone := types.Phone(strAcc[1])
+
+		balance, err := strconv.ParseInt(strAcc[2], 10, 64)
+		if err != nil {
+			log.Print(err)
+			return err
+		}
+
+		account := &types.Account{
+			ID:      id,
+			Phone:   phone,
+			Balance: types.Money(balance),
+		}
+
+		s.accounts = append(s.accounts, account)
+		log.Print(account)
 	}
 	return nil
 }
