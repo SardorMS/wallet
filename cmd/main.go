59fcc8184a1c0de99c4bc0c1cb9cd9f6943e5e1d
SardorMS <mshinodawindows@gmail.com>
2021-04-23 22:47:25 +0500

    f

--- cmd/main.go
+++ cmd/main.go
@@ -22,20 +22,20 @@ func main() {
 	svc.RegisterAccount("1111")
 	svc.Deposit(1, 500)
 	pay, _ := svc.Pay(1, 100, "phone")
-	t, _ := svc.FavoritePayment(pay.ID, "my_phone")
-	svc.PayFromFavorite(t.ID)
+	svc.FavoritePayment(pay.ID, "my_phone")
+	// svc.PayFromFavorite(t.ID)
 	//================================================
 	svc.RegisterAccount("2222")
 	svc.Deposit(2, 1000)
 	pay1, _ := svc.Pay(2, 200, "auto")
-	t1, _ := svc.FavoritePayment(pay1.ID, "my_auto")
-	svc.PayFromFavorite(t1.ID)
+	svc.FavoritePayment(pay1.ID, "my_auto")
+	// svc.PayFromFavorite(t1.ID)
 	//=================================================
 	svc.RegisterAccount("3333")
 	svc.Deposit(3, 12000)
 	pay2, _ := svc.Pay(3, 300, "shop")
-	t2, _ := svc.FavoritePayment(pay2.ID, "my_shop")
-	svc.PayFromFavorite(t2.ID)
+	svc.FavoritePayment(pay2.ID, "my_shop")
+	// svc.PayFromFavorite(t2.ID)
 
 	//==================================================
 	svc.Export("../data")
