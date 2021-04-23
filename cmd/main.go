1400ff6978ee22495e7ed4c0a7ada1dcdff6b8e6
SardorMS <mshinodawindows@gmail.com>
2021-04-23 21:43:50 +0500

    commit changes

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/SardorMS/wallet/pkg/wallet"
-	"log"
 )
 
 func main() {
@@ -22,33 +21,25 @@ func main() {
 	// ==================================================
 	svc.RegisterAccount("1111")
 	svc.Deposit(1, 500)
-	pay, err := svc.Pay(1, 100, "phone")
-	if err != nil {
-		log.Print(err)
-		return
-	}
-	svc.FavoritePayment(pay.ID, "my_phone")
+	pay, _ := svc.Pay(1, 100, "phone")
+	t, _ := svc.FavoritePayment(pay.ID, "my_phone")
+	svc.PayFromFavorite(t.ID)
 	//================================================
 	svc.RegisterAccount("2222")
-	svc.Deposit(2, 800)
-	pay1, err := svc.Pay(2, 200, "auto")
-	if err != nil {
-		log.Print(err)
-		return
-	}
-	svc.FavoritePayment(pay1.ID, "my_auto")
+	svc.Deposit(2, 1000)
+	pay1, _ := svc.Pay(2, 200, "auto")
+	t1, _ := svc.FavoritePayment(pay1.ID, "my_auto")
+	svc.PayFromFavorite(t1.ID)
 	//=================================================
 	svc.RegisterAccount("3333")
-	svc.Deposit(3, 1000)
-	pay2, err := svc.Pay(3, 300, "shop")
-	if err != nil {
-		log.Print(err)
-		return
-	}
-	svc.FavoritePayment(pay2.ID, "my_shop")
+	svc.Deposit(3, 12000)
+	pay2, _ := svc.Pay(3, 300, "shop")
+	t2, _ := svc.FavoritePayment(pay2.ID, "my_shop")
+	svc.PayFromFavorite(t2.ID)
+
 	//==================================================
-	// svc.Export("../data")
-	// svc.Import("../data")
+	svc.Export("../data")
+	svc.Import("../data")
 	// svc.ExportToFile("../data/export.txt")
 	// svc.ImportFromFile("../data/export.txt")
 }
